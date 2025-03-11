827286f1ad74f76fd5b7f0e2487c26a4cbe55b6a
TerribleJavaProgrammer <abdulrahman42908@gmail.com>
2025-03-10 23:41:59 -0400

    Create draw.js

---
+++ draw.js
@@ -0,0 +1,96 @@
+import { createBlockGradient } from './shader.js';
+import {
+  canvas,
+  ctx,
+  nextCanvas,
+  nextCtx,
+  heldCanvas,
+  heldCtx,
+  scoreDisplay,
+  linesDisplay,
+  ROWS,
+  COLUMNS,
+  game,
+  colorBoard,
+  pieceColors,
+  colorNameToHex
+} from './constants.js';
+
+export function drawGame(nextPiece, heldPiece) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < ROWS; i++) {
+        for (let j = 0; j < COLUMNS; j++) {
+            if (i !== ROWS - 1) {
+                if (game[i][j] !== 0 && colorBoard[i][j]) {
+                    drawBlock(j, i, colorNameToHex[colorBoard[i][j]], ctx);
+                } else {
+                    drawBlock(j, i, 'black', ctx);
+                }
+            }
+        }
+    }
+    drawPiecePreview(nextPiece, nextCtx, nextCanvas);
+    drawPiecePreview(heldPiece, heldCtx, heldCanvas);
+}
+
+export function updateDisplay(score, clearedLines) {
+    scoreDisplay.textContent = `Score: ${score}`;
+    linesDisplay.textContent = `Lines: ${clearedLines}`;
+}
+
+function drawBlock(x, y, color, ctx) {
+    const blockSize = 35;
+    if (color == 'black') {
+        ctx.fillStyle = color;
+        ctx.fillRect(x * blockSize, y * blockSize, blockSize, blockSize);
+        ctx.strokeStyle = 'darkgray';
+        ctx.strokeRect(x * blockSize, y * blockSize, blockSize, blockSize);
+    } else {
+        const gradient = createBlockGradient(ctx, x * blockSize, y * blockSize, color, blockSize);
+        ctx.fillStyle = gradient;
+        ctx.fillRect(x * blockSize, y * blockSize, blockSize, blockSize);
+        ctx.strokeStyle = 'black';
+        ctx.strokeRect(x * blockSize, y * blockSize, blockSize, blockSize);
+    }
+}
+
+function drawPiecePreview(piece, context, canvas) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    if (!piece) return;
+    const pieceType = piece.getType();
+    let arr;
+    switch (pieceType) {
+        case "O":
+            arr = [[0, 0, 0], [0, 1, 1], [0, 1, 1], [0, 0, 0]];
+            break;
+        case "I":
+            arr = [[0, 1], [0, 1], [0, 1], [0, 1]];
+            break;
+        case "S":
+            arr = [[0, 0, 0], [0, 1, 1], [1, 1, 0], [0, 0, 0]];
+            break;
+        case "Z":
+            arr = [[0, 0, 0], [1, 1, 0], [0, 1, 1], [0, 0, 0]];
+            break;
+        case "L":
+            arr = [[0, 1, 0], [0, 1, 0], [0, 1, 1]];
+            break;
+        case "J":
+            arr = [[0, 0, 1], [0, 0, 1], [0, 1, 1]];
+            break;
+        case "T":
+            arr = [[0, 0, 0], [1, 1, 1], [0, 1, 0], [0, 0, 0]];
+            break;
+        default:
+            return;
+    }
+    
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[0].length; j++) {
+            if (arr[i][j] == 1) {
+                let color = colorNameToHex[pieceColors[pieceType]];
+                drawBlock(j * 35, i * 35, color, ctx);
+            }
+        }
+    }
+}
